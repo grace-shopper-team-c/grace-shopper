6ee9a27253286e075a40a96a4599e3b173669d91
Marie Crumpton <marie.k.cr@gmail.com>
2020-09-22 12:30:49 -0400

    test: Added tests to seed

--- script/seed.spec.js
+++ script/seed.spec.js
@@ -2,7 +2,16 @@
 /* global describe it */
 
 const seed = require('./seed')
+const {Item, User} = require('../server/db')
 
 describe('seed script', () => {
   it('completes successfully', seed)
+  it('populates the database with at least four items', async () => {
+    const seedItems = await Item.findAll()
+    expect(seedItems).to.have.lengthOf.at.least(4)
+  })
+  it('populates the database with at least two users', async () => {
+    const seedUsers = await User.findAll()
+    expect(seedUsers).to.have.lengthOf.at.least(2)
+  })
 })
